8298bb041ef4d279050e2f0bb1c4293f6251d42a
Dzmitry-Radziuk <mitia.radiuk@yandex.ru>
2025-07-14 21:21:35 +0300

    Final version 1.0 Внесены правки для прохождения тестов.#2

--- settings.py
+++ settings.py
@@ -7,4 +7,5 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
     SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SERVER_NAME = os.getenv("SERVER_NAME", "localhost:5000")
+    #Для локальных тестов
+    #SERVER_NAME = os.getenv("SERVER_NAME", "localhost:5000")
